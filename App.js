f23f3f1f1bbcdc5b197b8da380cb72da51809ab2
Nagai <wakoto3@gmail.com>
2024-02-13 10:40:03 +0900

    animation再生

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import React, { Suspense } from 'react'
-import { useFrame, Canvas } from '@react-three/fiber/native'
-import { useGLTF, Environment } from '@react-three/drei/native'
-import iphoneModelPath from './assets/avaturn_demo_avatar_01.glb'
+import React, { Suspense ,useEffect, useRef} from 'react'
+import { Canvas ,useFrame } from '@react-three/fiber/native'
+import { useGLTF, useAnimations } from '@react-three/drei/native'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
+
+import modelPath from './assets/Ochiai_F2_20240117_v1.glb'
+
 
 function Model({ url, ...rest }) {
-  const { scene } = useGLTF(url)
-  useFrame(() => (scene.rotation.y += 0.01))
-  return <primitive {...rest} object={scene} />
+  const group = useRef();
+  const { scene, animations } = useGLTF(url, true, GLTFLoader);
+  const { actions ,names } = useAnimations(animations, group)
+
+
+
+  console.log("###################" );
+  console.log(scene);
+  console.log("###################" );
+
+  console.log("###################" );
+  console.log(animations);
+  console.log("###################" );
+
+  console.log("###################" );
+  console.log(actions);
+  console.log("###################" );
+
+
+  names.forEach(element => {
+    console.log("###################" );
+    console.log("name : " + element);
+    console.log("###################" );
+  });
+
+  const mixer = useRef();
+
+  useEffect(() => {
+    mixer.current = new THREE.AnimationMixer(scene);
+    const action = mixer.current.clipAction(animations[0]); // Assuming the first animation is used
+    action.play();
+
+    return () => {
+      mixer.current.stopAllAction();
+    };
+  }, [animations, scene]);
+
+  useFrame((_, delta) => {
+    mixer.current.update(delta);
+  });
+
+  // actions.forEach(element => {
+  //   console.log("name : " + element);
+  // });
+
+  // useEffect(() => {
+  //   // モデルがロードされた後にアニメーションを再生する
+  //   if ( actions ) {
+  //       actions[names[0]].play();
+  //   }
+  // }, [actions, names,mixer]);
+
+  // useFrame(() => (scene.rotation.y += 0.01))
+  return <primitive {...rest} ref={group} object={scene} />
 }
 
 export default function App() {
@@ -17,7 +72,7 @@ export default function App() {
       <directionalLight intensity={1.1} position={[0.5, 0, 0.866]} />
       <directionalLight intensity={0.8} position={[-6, 2, 2]} />
       <Suspense>
-        <Model url={iphoneModelPath} />
+        <Model url={modelPath} />
       </Suspense>
     </Canvas>
   )
